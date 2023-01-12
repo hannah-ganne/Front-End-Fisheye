baf36b35f9a7dd7a262ed713468ed46370bbff3e
lanomade <ganne.hannah@gmail.com>
2023-01-12 16:08:35 +0100

    add createPhotographerCard function

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,42 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const article = document.createElement('article');
+
+        const AllPhotographers = `
+            <a href="./photographer.html?id=${id}">
+                <img src=${picture} alt='photo of ${name}' />
+                <h2>${name}</h2>
+            </a>
+            <h3>${city}, ${country}</h3>
+            <p>${tagline}</p>
+            <span>${price}€/jour</span>
+        `;
+
+        article.innerHTML = AllPhotographers;
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+
+    function createPhotographerCard() {
+        const header = document.querySelector('.photograph-header');
+
+        const Photographer = `
+            <section class="photograph-info">
+                <h1>${name}</h1>
+                <h3>${city}, ${country}</h3>
+                <p>${tagline}</p>
+            </section>
+            <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
+            <img src=${picture} alt='photo of ${name}' />
+        `
+        header.innerHTML = Photographer
+
+        return header
+    }
+
+    return { name, picture, getUserCardDOM, createPhotographerCard }
 }
