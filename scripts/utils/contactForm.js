1020d697ace0cf0c41b4443cd2363d5aaf05f85f
lanomade <ganne.hannah@gmail.com>
2023-01-20 12:06:26 +0100

    add functions for modal control

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,48 @@
 function displayModal() {
+    const body = document.getElementById("body");
+    const main = document.getElementById("main");
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    const closeBtn = document.getElementById("closeBtn");
+
+    modal.style.display = "block";
+    body.classList.add('no-scroll');
+    closeBtn.focus();
+    main.setAttribute('aria-hidden', 'true');
+    modal.setAttribute('aria-hidden', 'false');
 }
 
 function closeModal() {
+    const body = document.getElementById("body");
+    const main = document.getElementById("main");
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
+    body.classList.remove('no-scroll');
+    main.setAttribute('aria-hidden', 'false');
+    modal.setAttribute('aria-hidden', 'true');
 }
+
+function sendForm() {
+    const form = document.getElementById("form");
+    const firstName = document.getElementById("first-name");
+    const lastName = document.getElementById("last-name");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+
+    let contactDetails = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        email: email.value,
+        message: message.value
+    }
+
+    console.log(contactDetails);
+
+    closeModal();
+}
+
+const modal = document.getElementById("contact_modal");
+modal.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+        closeModal()
+    }
+})
