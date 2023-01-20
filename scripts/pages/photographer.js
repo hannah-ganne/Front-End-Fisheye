2929721f4312ea8eaa22d80713789586c5e0a712
lanomade <ganne.hannah@gmail.com>
2023-01-20 12:06:06 +0100

    add displayPriceBanner function

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -31,19 +31,37 @@ async function getMedia() {
 }
 
 async function displayPhotographerCard(photographer) {
-    const photographerModel = photographerFactory(photographer[0]);
-    const photographerCard = photographerModel.createPhotographerCard();
+    const photographerModel = new photographerFactory(photographer[0]);
+    photographerModel.createPhotographerCard();
 };
 
 async function displayMediaCards(media) {
     const mediaSection = document.querySelector(".media-section")
 
     media.map(item => {
-        const mediaModel = mediaFactory(item);
+        const mediaModel = new mediaFactory(item);
         const mediaCard = mediaModel.createMediaCard();
         mediaSection.appendChild(mediaCard);
     });
+}
 
+async function displayPriceBanner(photographer, media) {
+    const main = document.getElementById("main");
+
+    const photographerPrice = photographer[0].price;
+    const likes = media.map(m => m.likes)
+    const totalLikeCount = likes.reduce((a, b) => a + b, 0)
+
+    const priceBanner = `
+        <span>${totalLikeCount} <img src="assets/icons/black-heart.svg" alt="heart icon" class="heart-icon" /></span>
+        <span>${photographerPrice}€ / jour</span>
+    `
+    const div = document.createElement("div");
+    div.className = "price-banner";
+    div.setAttribute("aria-label", "price banner");
+
+    div.innerHTML = priceBanner;
+    main.appendChild(div);
 
 }
 
@@ -54,6 +72,7 @@ async function init() {
     const media = await getMedia();
     displayPhotographerCard(photographer);
     displayMediaCards(media);
+    displayPriceBanner(photographer, media);
 };
 
 init();
