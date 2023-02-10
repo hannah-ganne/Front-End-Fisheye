d01bd3f6da08644386780454d1ae1d79c567e342
lanomade <ganne.hannah@gmail.com>
2023-02-10 11:41:51 +0100

    add lightbox title

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,7 +1,7 @@
-// const media = document.getElementsByClassName("card-media");
 let currentIndex;
 const nextBtn = document.getElementById('next-lightbox');
 const previousBtn = document.getElementById('previous-lightbox');
+const lightboxModal = document.getElementById('lightbox-modal');
 
 function openLightbox(index) {
     const main = document.getElementById("main");
@@ -9,6 +9,7 @@ function openLightbox(index) {
     const closeBtn = document.getElementById("close-lightbox");
     const modalWrapper = document.querySelector(".modal-wrapper");
     const media = document.querySelector(`.media[data-index="${index}"]`);
+    const mediaTitle = document.querySelector(`.media-title[data-index="${index}"]`)
 
     currentIndex = index;
     lightbox.style.display = "block";
@@ -17,6 +18,7 @@ function openLightbox(index) {
     main.setAttribute('aria-hidden', 'true');
     lightbox.setAttribute('aria-hidden', 'false');
     media.style.display = "block";
+    mediaTitle.style.display = "block";
 }
 
 function closeLightbox() {
@@ -24,16 +26,20 @@ function closeLightbox() {
     const lightbox = document.getElementById("lightbox-modal");
     const modalWrapper = document.querySelector(".modal-wrapper");
     const media = document.querySelectorAll(".media");
+    const mediaTitle = document.querySelectorAll(".media-title");
+
 
     lightbox.style.display = "none";
     modalWrapper.style.visibility = "hidden"
     main.setAttribute('aria-hidden', 'false');
     lightbox.setAttribute('aria-hidden', 'true');
     media.forEach(item => item.style.display = "none")
+    mediaTitle.forEach(item => item.style.display = "none")
 }
 
 function goToNext() {
     const allMedia = document.querySelectorAll(".media");
+    const allMediaTitles = document.querySelectorAll(".media-title");
 
     if (currentIndex == allMedia.length - 1) {
         currentIndex = 0
@@ -42,9 +48,12 @@ function goToNext() {
     }
 
     const next = document.querySelector(`.media[data-index="${currentIndex}"]`);
+    const nextTitle = document.querySelector(`.media-title[data-index="${currentIndex}"]`)
 
     allMedia.forEach(item => item.style.display = "none")
+    allMediaTitles.forEach(item => item.style.display = "none")
     next.style.display = "block";
+    nextTitle.style.display = "block";
 }
 
 function goToPrevious() {
@@ -62,26 +71,19 @@ function goToPrevious() {
     previous.style.display = "block";
 }
 
-// media.forEach(item => item.addEventListener('click', (event) => {
-//     event.stopPropagation();
-//     const index = event.target.getAttribute('data-index');
-//     currentIndex = index;
-//     openLightbox(index);
-// }))
-
 nextBtn.addEventListener('click', goToNext);
 previousBtn.addEventListener('click', goToPrevious);
-document.addEventListener('keydown', (event) => {
+lightboxModal.addEventListener('keydown', (event) => {
     if (event.code === 'ArrowRight') {
         goToNext()
     }
 })
-document.addEventListener('keydown', (event) => {
+lightboxModal.addEventListener('keydown', (event) => {
     if (event.code === 'ArrowLeft') {
         goToPrevious()
     }
 })
-document.addEventListener('keydown', (event) => {
+lightboxModal.addEventListener('keydown', (event) => {
     if (event.code === 'Escape') {
         closeLightbox()
     }
