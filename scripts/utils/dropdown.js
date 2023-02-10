33e379c987a624e84b83d4607911f6c9649c282e
lanomade <ganne.hannah@gmail.com>
2023-02-10 11:41:22 +0100

    add dropdown menu logic

---
+++ scripts/utils/dropdown.js
@@ -0,0 +1,26 @@
+let isOpen = false;
+const dropDown = document.querySelector('.dropdown');
+const dropDownBtn = document.querySelector('.dropdown-toggle')
+const dropDownMenu = document.querySelector('.dropdown-menu');
+
+function toggle() {
+    if (!isOpen) {
+        dropDownMenu.style.visibility = 'visible'
+        isOpen = true;
+    } else {
+        dropDownMenu.style.visibility = 'hidden'
+        isOpen = false;
+    }
+}
+
+function setValue(value) {
+    dropDownBtn.textContent = value
+}
+
+dropDown.addEventListener('click', toggle);
+[...dropDownMenu.children].forEach(list => {
+    list.addEventListener('click', () => {
+        const value = list.getAttribute('data-value');
+        setValue(value)
+    })
+})
