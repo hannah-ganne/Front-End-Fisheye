f3d9c0cce249f3243c259c2f374a2f27086cff16
lanomade <ganne.hannah@gmail.com>
2023-04-26 14:06:57 +0200

    fix formatting

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,54 +1,53 @@
 function openModal() {
-    const body = document.getElementById("body");
-    const main = document.getElementById("main");
-    const modal = document.getElementById("contact-modal");
-    const closeBtn = document.getElementById("close-modal");
-    const modalWrapper = document.querySelector(".modal-wrapper");
+  const body = document.getElementById('body');
+  const main = document.getElementById('main');
+  const modal = document.getElementById('contact-modal');
+  const closeBtn = document.getElementById('close-modal');
+  const modalWrapper = document.querySelector('.modal-wrapper');
 
-    modal.style.display = "block";
-    modalWrapper.style.visibility = "visible"
-    body.classList.add('no-scroll');
-    closeBtn.focus();
-    main.setAttribute('aria-hidden', 'true');
-    modal.setAttribute('aria-hidden', 'false');
+  modal.style.display = 'block';
+  modalWrapper.style.visibility = 'visible';
+  body.classList.add('no-scroll');
+  closeBtn.focus();
+  main.setAttribute('aria-hidden', 'true');
+  modal.setAttribute('aria-hidden', 'false');
 }
 
 function closeModal() {
-    const body = document.getElementById("body");
-    const main = document.getElementById("main");
-    const modal = document.getElementById("contact-modal");
-    const modalWrapper = document.querySelector(".modal-wrapper");
+  const body = document.getElementById('body');
+  const main = document.getElementById('main');
+  const modal = document.getElementById('contact-modal');
+  const modalWrapper = document.querySelector('.modal-wrapper');
 
-
-    modal.style.display = "none";
-    modalWrapper.style.visibility = "hidden"
-    body.classList.remove('no-scroll');
-    main.setAttribute('aria-hidden', 'false');
-    modal.setAttribute('aria-hidden', 'true');
+  modal.style.display = 'none';
+  modalWrapper.style.visibility = 'hidden';
+  body.classList.remove('no-scroll');
+  main.setAttribute('aria-hidden', 'false');
+  modal.setAttribute('aria-hidden', 'true');
 }
 
 function sendForm() {
-    const form = document.getElementById("form");
-    const firstName = document.getElementById("first-name");
-    const lastName = document.getElementById("last-name");
-    const email = document.getElementById("email");
-    const message = document.getElementById("message");
+  const form = document.getElementById('form');
+  const firstName = document.getElementById('first-name');
+  const lastName = document.getElementById('last-name');
+  const email = document.getElementById('email');
+  const message = document.getElementById('message');
 
-    let contactDetails = {
-        firstName: firstName.value,
-        lastName: lastName.value,
-        email: email.value,
-        message: message.value
-    }
+  const contactDetails = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: email.value,
+    message: message.value,
+  };
 
-    console.log(contactDetails);
+  console.log(contactDetails);
 
-    closeModal();
+  closeModal();
 }
 
-const modal = document.getElementById("contact-modal");
-modal.addEventListener('keydown', event => {
-    if (event.key === 'Escape') {
-        closeModal()
-    }
-})
+const modal = document.getElementById('contact-modal');
+modal.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+});
