be9f787c1ce9d3af700e2ad22e34227cea004b9b
lanomade <ganne.hannah@gmail.com>
2023-04-26 14:06:37 +0200

    refactor the async function

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,30 +1,31 @@
 async function getPhotographers() {
-
-    let photographers = await fetch('/data/photographers.json')
-        .then(res => res.json())
-        .then(data => data.photographers)
-        .catch(error => console.log(error))
-
-    return ({
-        photographers: [...photographers]
-    })
+  try {
+    const res = await fetch('/data/photographers.json');
+    const data = await res.json();
+    const {photographers} = data;
+    return {
+      photographers: [...photographers]
+    };
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 }
 
 async function displayData(photographers) {
-    const photographersSection = document.querySelector(".photographer_section");
+  const photographersSection = document.querySelector('.photographer_section');
 
-    photographers.map((photographer) => {
-        const photographerModel = new photographerFactory(photographer);
-        const userCardDOM = photographerModel.getUserCardDOM();
-        photographersSection.appendChild(userCardDOM);
-    });
-};
+  photographers.map((photographer) => {
+    const photographerModel = new photographerFactory(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
 
 async function init() {
-    // Récupère les datas des photographes
-    const { photographers } = await getPhotographers();
-    displayData(photographers);
-};
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+}
 
 init();
-
